4ed17fd9acf730165259402004eda227003eff2f
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2023-02-25 18:24:07 -0600

    Changed spell-checker program, from aspell to hunspell.
    Hunspell allows you to check multiple languages at the same time.

--- core/corvus-behaviour.el
+++ core/corvus-behaviour.el
@@ -38,7 +38,26 @@
 
 ;; Spell checking on buffers.
 (require 'flyspell)
-(setq ispell-program-name "aspell")
+
+(with-eval-after-load "ispell"
+  ;; Configure 'LANG', otherwise ispell.el cannot find a 'default-dictionary'
+  ;; even though multiple dictionaries will be configured in next line.
+  (setenv "LANG" "en_US.UTF-8")
+  (setq ispell-program-name "hunspell")
+  ;; Configure the necessary languages.
+  (setq ispell-dictionary "en_GB,en_US,es_MX")
+  ;; ispell-set-spellchecker-params has to be called before
+  ;; ispell-hunspell-add-multi-dic to work.
+  (ispell-set-spellchecker-params)
+  (ispell-hunspell-add-multi-dic "en_GB,en_US,es_MX")
+  ;; Saving words to the personal dictionary.
+  (setq ispell-personal-dictionary "~/.hunspell_personal")
+  )
+
+;; The personal dictionary file has to exist, otherwise hunspell will
+;; silently not use it.
+(unless (file-exists-p ispell-personal-dictionary)
+  (write-region "" nil ispell-personal-dictionary nil 0))
 
 ;; Spell checking on texts.
 (add-hook 'text-mode-hook #'flyspell-mode)
